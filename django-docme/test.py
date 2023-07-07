bbefe0c868479c7b2e70c140a614565f9337ce37
Ezzoubair <98271602+zarqizoubir@users.noreply.github.com>
2023-07-07 18:32:43 +0100

    ci/cd testing

---
+++ django-docme/test.py
@@ -0,0 +1,26 @@
+import sys
+import time
+
+print("Test 1 : testing ...", file=sys.stderr)
+time.sleep(1)
+print("test 1 passed")
+
+
+print("Test 2 : testing ...", file=sys.stderr)
+time.sleep(1)
+print("test 2 passed")
+
+
+print("Test 3 : testing ...", file=sys.stderr)
+time.sleep(1)
+print("test 3 passed")
+
+
+print("Test 4 : testing ...", file=sys.stderr)
+time.sleep(1)
+print("test 4 passed")
+
+
+print("Test 5 : testing ...", file=sys.stderr)
+time.sleep(1)
+print("test 5 passed")
