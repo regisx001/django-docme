f7946f3db8c14fc3fb3160fbfc2b9966fa3ad92d
Ezzoubair <98271602+zarqizoubir@users.noreply.github.com>
2023-07-11 01:09:21 +0100

    test deploy

--- website/src/routes/(docs)/docs/+page.svelte
+++ website/src/routes/(docs)/docs/+page.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import Docs from '$lib/Components/Docs/Docs.svelte';
+	import { introduction } from '$lib/Content/introduction';
 	import { dataUrl, current_title } from '$lib/stores';
 	import { onMount } from 'svelte';
 
 	let data: any;
 	$: {
-		fetch('/Content/' + $dataUrl).then(async (res) => {
+		fetch(base + '/Content/' + $dataUrl).then(async (res) => {
 			data = await res.text();
 		});
 	}
+	// onMount(async () => {
+	// 	data = (await import('$lib/Content/introduction')).introduction;
+	// });
 </script>
 
 <svelte:head>
 	<title>{$current_title} | Django docme Docs</title>
 </svelte:head>
 
-<!-- <Docs {data} /> -->
-
-{data}
+<Docs {data} />
